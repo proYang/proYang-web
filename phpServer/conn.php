55cf7a4cf1bc44dec706c6b373850ec457282039
proYang <904509221@qq.com>
2016-03-05 12:52:55 +0800

    2016/3/5
    备用方案

--- phpServer/conn.php
+++ phpServer/conn.php
@@ -1,4 +1,4 @@
-﻿<?php
-    $link = mysqli_connect('mysql.hostinger.com.hk', 'u277374159_root', 'r5Tbqe4jl3','u277374159_blog');//链接数据库
+<?php
+    $link = mysqli_connect('localhost', 'root', '','myblog');//链接数据库
     $result = mysqli_select_db($link,"myblog");
     mysqli_query($link,'SET NAMES UTF8');
