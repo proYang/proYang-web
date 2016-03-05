98b133aab83c5895f8872f62cde64e63f0e4cd19
proYang <904509221@qq.com>
2016-03-05 12:56:04 +0800

    测试
    备份

--- phpServer/conn_db.php
+++ phpServer/conn_db.php
@@ -1,7 +1,9 @@
 <?php
-@ $db = new mysqli('localhost', 'root', '', 'myblog');
+@ $db = new mysqli('mysql.hostinger.com.hk', 'u277374159_root', 'r5Tbqe4jl3','u277374159_blog');
 $db->query('set names utf8');
 if(mysqli_connect_errno()){
 	echo "Error: Could not connect to database. Please try again later";
 	exit;
 }
+
+?>
